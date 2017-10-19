35528a4fa82761efc904b099b61a554de652b90c
jerryrtz <32171513+jerryrtz@users.noreply.github.com>
2017-10-19 08:15:30 -0700

    Add files via upload

---
+++ Diamond.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Diamond {
+
+	
+	
+	      public static void main(String[] args) {
+	
+	 
+	
+	        Scanner input=new Scanner(System.in);
+	
+	        System.out.print("Enter number of rows. ");
+	
+	        int N=input.nextInt();
+	
+	        for(int i=1;i<=N;i++)       {
+	        for(int j=0;j<N-i;j++)System.out.print(" ");
+	
+	        for(int j=0;j<(2*i-1);j++) System.out.print("*");
+	
+	        System.out.println();
+	
+	          
+	
+	      }
+	}
+}
